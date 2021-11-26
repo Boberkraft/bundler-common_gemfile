44771a5aef3bffadf2e76d3264b5d9a0488d97a9
Andrzej Bisewski <andrzej.bisewski@gmail.com>
2021-11-26 22:48:19 +0100

    nowy bundler

--- lib/bundler-common_gemfile.rb
+++ lib/bundler-common_gemfile.rb
@@ -6,8 +6,8 @@ module Bundler
       while true
         number_of_evaled = evaled.size
         sources = context.instance_variable_get('@sources').all_sources
-        sources.select { |s| s.is_a?(Bundler::Source::Git) || s.is_a?(Bundler::Source::Path) }.each do |source|
 
+        sources.select { |s| s.is_a?(::Bundler::Source::Git) || s.is_a?(::Bundler::Source::Path) }.each do |source|
           source.instance_variable_set('@allow_cached', true)
           gem_path = if source.respond_to?('install_path')
                        source.install_path
@@ -15,19 +15,19 @@ module Bundler
                        source.expanded_original_path
                      end
 
-          gemfile_common_path = gem_path.join('Gemfile-common.rb')
-
           if !File.exist?(gem_path) && source.is_a?(Bundler::Source::Git)
             puts "  fetching \e[1m\e[92m#{source.name}\e[0m is search of common-gemfile"
             source.specs
           end
+
+          gemfile_common_path = gem_path.join('Gemfile-common.rb')
           if !evaled.include?(gemfile_common_path) && File.exist?(gemfile_common_path)
             evaled << gemfile_common_path
-            puts "Evaling \e[1m\e[92m#{gemfile_common_path}\e[0m"
+            STDERR.puts "Evaling \e[1m\e[92m#{gemfile_common_path}\e[0m"
             content = File.read(gemfile_common_path)
-            puts content if ENV['DEBUG_GEMFILE_COMMOM']
+            $stderr.puts content if ENV['DEBUG_GEMFILE_COMMON']
             context.send(:eval, content)
-            puts "Evaling \e[1m\e[92m#{'done'}\e[0m"
+            $stderr.puts "Evaling \e[1m\e[92m#{'done'}\e[0m"
           end
         end
 
