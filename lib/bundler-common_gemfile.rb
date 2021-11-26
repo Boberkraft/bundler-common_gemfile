6f3099bc84386c9b9b721e25d5abd4cd2c76c4d0
Andrzej Bisewski <andrzej.bisewski@gmail.com>
2021-11-26 23:00:44 +0100

    :art:

--- lib/bundler-common_gemfile.rb
+++ lib/bundler-common_gemfile.rb
@@ -1,5 +1,36 @@
+module ColoredStrings
+  refine String do
+    def black;          "\e[30m#{self}\e[0m" end
+    def red;            "\e[31m#{self}\e[0m" end
+    def green;          "\e[32m#{self}\e[0m" end
+    def brown;          "\e[33m#{self}\e[0m" end
+    def blue;           "\e[34m#{self}\e[0m" end
+    def magenta;        "\e[35m#{self}\e[0m" end
+    def cyan;           "\e[36m#{self}\e[0m" end
+    def gray;           "\e[37m#{self}\e[0m" end
+    def yellow;         "\e[93m#{self}\e[0m" end
+
+    def bg_black;       "\e[40m#{self}\e[0m" end
+    def bg_red;         "\e[41m#{self}\e[0m" end
+    def bg_green;       "\e[42m#{self}\e[0m" end
+    def bg_brown;       "\e[43m#{self}\e[0m" end
+    def bg_blue;        "\e[44m#{self}\e[0m" end
+    def bg_magenta;     "\e[45m#{self}\e[0m" end
+    def bg_cyan;        "\e[46m#{self}\e[0m" end
+    def bg_gray;        "\e[47m#{self}\e[0m" end
+
+    def bold;           "\e[1m#{self}\e[22m" end
+    def italic;         "\e[3m#{self}\e[23m" end
+    def underline;      "\e[4m#{self}\e[24m" end
+    def blink;          "\e[5m#{self}\e[25m" end
+    def reverse_color;  "\e[7m#{self}\e[27m" end
+  end
+end
+
 module Bundler
   class CommonGemfile
+    using ColoredStrings
+
     def self.load_other(context)
       evaled = []
 
@@ -8,26 +39,26 @@ module Bundler
         sources = context.instance_variable_get('@sources').all_sources
 
         sources.select { |s| s.is_a?(::Bundler::Source::Git) || s.is_a?(::Bundler::Source::Path) }.each do |source|
-          source.instance_variable_set('@allow_cached', true)
+          source.instance_variable_set('@allow_cached', true) # allow git commands
           gem_path = if source.respond_to?('install_path')
-                       source.install_path
+                       source.install_path # remote gem
                      else
-                       source.expanded_original_path
+                       source.expanded_original_path # local gem
                      end
 
           if !File.exist?(gem_path) && source.is_a?(Bundler::Source::Git)
-            puts "  fetching \e[1m\e[92m#{source.name}\e[0m is search of common-gemfile"
+            puts "  fetching #{source.name.to_s.green.bold} in search of common-gemfile"
             source.specs
           end
 
           gemfile_common_path = gem_path.join('Gemfile-common.rb')
           if !evaled.include?(gemfile_common_path) && File.exist?(gemfile_common_path)
             evaled << gemfile_common_path
-            STDERR.puts "Evaling \e[1m\e[92m#{gemfile_common_path}\e[0m"
+            STDERR.puts "Loading #{gemfile_common_path.to_s.green.bold}"
             content = File.read(gemfile_common_path)
             $stderr.puts content if ENV['DEBUG_GEMFILE_COMMON']
             context.send(:eval, content)
-            $stderr.puts "Evaling \e[1m\e[92m#{'done'}\e[0m"
+            $stderr.puts 'ok'.green.bold
           end
         end
 
